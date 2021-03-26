78b4d4bdcce611f219b87d7fe511034618f63a94
Julia Mitas <juliamitas.lote@gmail.com>
2021-03-26 16:07:15 +0100

    Made some basic operations with sample files.

--- samples.py
+++ samples.py
@@ -4,3 +4,8 @@ import mne
 import matplotlib.pyplot as plt
 import sklearn
 
+sample_data_folder = mne.datasets.sample.data_path()
+sample_data_raw_file = os.path.join(sample_data_folder, 'MEG', 'sample', 'sample_audvis_raw.fif')
+
+raw = mne.io.read_raw_fif(sample_data_raw_file)
+raw.crop(tmax=60).load_data()
