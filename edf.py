858a961b36c6bcbd4816f9251f3f830bb00125bb
Julia Mitas <juliamitas.lote@gmail.com>
2021-03-26 16:06:43 +0100

    Made some basic operations with edf files.

--- edf.py
+++ edf.py
@@ -10,17 +10,4 @@ path1 = "D:/Data/Data/KT53.edf"
 
 raw = mne.io.read_raw_edf(path1)
 
-print(type(raw))
-
-
-# basic operations
-sampling_freq = raw.info['sfreq']
-start_stop_seconds = np.array([10, 12])
-start_sample, stop_sample = (start_stop_seconds * sampling_freq).astype(int)
-channel_index = 0
-raw_selection = raw[channel_index, start_sample:stop_sample]
-
-x = raw_selection[1]
-y = raw_selection[0].T
-plt.plot(x, y)
-plt.show()
+raw.crop(tmax=60).load_data()
